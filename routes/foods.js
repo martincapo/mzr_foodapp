bb649b33c61654c171d9fadbaf709a05b8a12621
martincapo <martincapo@yahoo.com>
2017-09-27 20:06:44 +0000

    food.js works fine with db connection

--- routes/foods.js
+++ routes/foods.js
@@ -8,19 +8,19 @@ const router  = express.Router();
 module.exports = (knex) => {
 
   router.get("/", (req, res) => {
-    res.json([
-      {name: "BBQ", price: 30},
-      {name: "asparagus", price: 15},
-      {name: "apples", price: 10},
-      {name: "avocado", price: 12},
-      {name: "Apple Juice",price: 5}
-    ])
-    // knex
-    //   .select("*")
-    //   .from("food")
-    //   .then((results) => {
-    //     res.json(results);
-    // });
+    // res.json([
+    //   {name: "BBQ", price: 30},
+    //   {name: "asparagus", price: 15},
+    //   {name: "apples", price: 10},
+    //   {name: "avocado", price: 12},
+    //   {name: "Apple Juice",price: 5}
+    // ])
+    knex
+      .select("*")
+      .from("food")
+      .then((results) => {
+        res.json(results);
+    });
   });
 
   return router;
