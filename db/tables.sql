dbda98ab537f579eee6e78fc2bc39a4e0d2e781d
Ricky Chan <rickysychan@gmail.com>
2017-09-28 00:42:31 +0000

    refactored db

--- db/tables.sql
+++ db/tables.sql
@@ -1,33 +1,37 @@
 CREATE TABLE users (
-  id          BIGSERIAL PRIMARY KEY,
-  email       text,
-  phoneNumber varchar,
-  password    varchar
+  id           BIGSERIAL PRIMARY KEY,
+  email        text,
+  phone_number text,
+  password     text
 );
 
 CREATE TABLE vendors (
-  id            BIGSERIAL  PRIMARY KEY,
+  id            integer    PRIMARY KEY,
   address       text       NOT NULL,
-  phoneNumber   varchar    NOT NULL,
-  password      varchar
+  phoneNumber   text       NOT NULL,
+  password      text
 );
 
 CREATE TABLE orders (
-  id         BIGSERIAL  PRIMARY KEY,
-  vendorid   BIGSERIAL  NOT NULL,
-  userid     BIGSERIAL  NOT NULL,
-  foodid     varchar    NOT NULL,
-  completed  boolean    NOT NULL
+  id         BIGSERIAL     PRIMARY KEY,
+  vendorid   Bigint        references vendors,
+  userid     Bigint        references users,
+  completed  boolean       NOT NULL
 );
 
 CREATE TABLE food (
   id            BIGSERIAL  PRIMARY KEY,
   name          text       NOT NULL,
   price         DECIMAl    NOT NULL,
-  vendorid      BIGSERIAL  NOT NULL,
+  vendorid      integer    NOT NULL,
   description   text       NOT NULL
 );
 
+CREATE TABLE users_orders (
+  id         BIGSERIAL     PRIMARY KEY,
+  orderid    Bigint        references orders,
+  foodid     Bigint        references food
+);
 
 
 
