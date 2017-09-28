8632e63eee37359e30e27401a70eeb880c0124de
martincapo <martincapo@yahoo.com>
2017-09-28 23:13:58 +0000

    orders by user is done. now we can get orders data of a particular user

--- routes/orders.js
+++ routes/orders.js
@@ -17,6 +17,20 @@ module.exports = (knex) => {
           res.json(results);
         });
   });
+
+  router.get("/:id", (req, res) => {
+    knex
+        .select(['orders.id', 'orders.user_id', 'orders.vendor_id', 'orders.completed',
+      'users.name AS user_name' , 'users.email AS user_email', 'users.phone_number AS user_phone_number',
+      'vendors.name AS vendor_name', 'vendors.address AS vendor_address', 'vendors.phone_number AS vendor_phone_number'])
+        .from('orders')
+        .leftJoin('vendors', 'orders.vendor_id', 'vendors.id')
+        .leftJoin('users', 'orders.user_id', 'users.id')
+        .where('orders.user_id', req.params.id)
+        .then((results) => {
+          res.json(results);
+        });
+  })
   return router;
 }
 
