8984f850d5603e3a62c8a15967aab30300da6b10
martincapo <martincapo@yahoo.com>
2017-09-29 17:41:42 +0000

    minor fix

--- routes/orders.js
+++ routes/orders.js
@@ -4,10 +4,17 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (knex) => {
+
+  route.post("/", (req, res) => {
+    knex('orders')
+    .insert({user_id: req.user_id})
+    .insert({vendor_id: req.vendor_id});
+  })
+
 // full list of orders
   router.get("/", (req, res) => {
     knex
-        .select(['orders.id', 'orders.user_id', 'orders.vendor_id', 'orders.est_mins','orders.completed',
+        .select(['orders.id', 'orders.user_id', 'orders.vendor_id', 'orders.est_mins','orders.completed', 'orders.order_date',
       'users.name AS user_name' , 'users.email AS user_email', 'users.phone_number AS user_phone_number',
       'vendors.name AS vendor_name', 'vendors.address AS vendor_address', 'vendors.phone_number AS vendor_phone_number'])
         .from('orders')
