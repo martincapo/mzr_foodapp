6a54eff2b68590b451451bc73bcb41abe5a722cd
martincapo <martincapo@yahoo.com>
2017-10-01 16:24:05 +0000

    Added process bar in orders list page

--- public/scripts/orders_history.js
+++ public/scripts/orders_history.js
@@ -37,6 +37,22 @@ $(function() {
       })
     }
 
+    const procesOrder = (min) => {
+      var current_progress = 0;
+      var interval = setInterval(function() {
+          current_progress += 10;
+          $("#dynamic")
+          .css("width", current_progress + "%")
+          .attr("aria-valuenow", current_progress)
+          .text(current_progress + "% Complete");
+          if (current_progress >= 100)
+              clearInterval(interval);
+      }, min*1000);
+    }
+
     ordersCallForUser($('.userID').text());
+    procesOrder(1);
 
 })
+
+
